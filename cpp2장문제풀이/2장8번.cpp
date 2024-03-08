301884bed788297f943a93d6210249dd4d37c55a
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-03-08 19:42:46 +0900

    Create 2장8번.cpp

---
+++ cpp2장문제풀이/2장8번.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string max_str;
+	char name[100];
+	int max = 0;
+	int max_index;
+	cout << "5 명의 이름을 ';'으로 구분하여 입력하세요\n";
+	for (int i = 1; i <= 5; i++) {               //for문 내부의 구조를 잘 알아야 함
+		cin.getline(name, 100, ';');             //입력받다가 ; 나오면 입력 끝
+		cout << i << " : " << name << '\n';     // 그 다음에 출력, 그 다음에 또 입력받다가 ; 나오면 입력 끝-> 출력
+		if (max < strlen(name)) {
+			max = strlen(name);
+			max_str = name;
+		}
+	}
+	cout << "가장 긴 이름은" << max_str;
+	return 0;
+}
