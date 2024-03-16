6e98bdf5c70eec830c2756aecbe1da5b852d34a9
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-03-16 20:26:04 +0900

    Update 3장2번.cpp
    코드 오류로 변경

--- cpp3장문제풀이/3장2번.cpp
+++ cpp3장문제풀이/3장2번.cpp
@@ -1,73 +1,47 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-/*
-
-stoi()함수가 숫자형태가 아닌 것이 있으면 빼고 그 앞까지 정수로 바꿔주는듯함.
-substr(a,b)은 a=추출시작인덱스, b=추출끝인덱스 b는 추출대상이 아님.
-
-*/
-
+// 이 전 코드는 month가 2자리이면 코드실행이 안되어 지금의 코드로 변경함
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Date{
-    private:
-    int year;
-    int month;
-    int day;
-    
-    public:
-    Date(int year, int month, int day);
-    Date(string YMD);
-    int getYear();
-    int getMonth();
-    int getDay(); 
-    void show();
+class Date {
+	int year;
+	int month;
+	int day;
+public:
+	Date(int y, int m, int d) {
+		year = y;
+		month = m;
+		day = d;
+	}
+	Date(string ymd);
+	int getYear();
+	int getMonth();
+	int getDay();
+	void show();
 };
-
-Date::Date(int x, int y, int z)
-{
-    year = x;
-    month = y;
-    day = z;
+Date::Date(string ymd) {
+	char NewYMD[11];
+	strcpy(NewYMD, ymd.c_str());   //c.str()함수는 string클래스의 문자열을 char* 형태의 문자열로 변환시켜주는 함수이다.
+	year = stoi(strtok(NewYMD, "/")); //strtok는 문자열을 해당 문자를 기준으로 잘라준다. 자르고 남은 문자열을 계속 자르겠다면 
+	month = stoi(strtok(NULL, "/"));  //문자열 매개변수를 NULL로 입력을 해준다.
+	day = stoi(strtok(NULL, "/"));
 }
 
-Date::Date(string YMD)
-{
-    year = stoi(YMD);
-	month = stoi(YMD.substr(5,6));
-	day = stoi(YMD.substr(7,9));
+int Date::getYear() {
+	return year;
 }
-
-int Date::getYear()
-{
-    return year;
+int Date::getMonth() {
+	return month;
 }
-
-int Date::getMonth()
-{
-    return month;
+int Date::getDay() {
+	return day;
 }
-
-int Date::getDay()
-{
-    return day;
+void Date::show() {
+	cout << year << "년" << month << "월" << day << "일" << endl;
 }
-
-void Date::show(){
-    cout << year << "년" << month << "월" << day << "일" <<'\n';
-}
-int main(){
-    Date birth(2014, 3, 20);
-    Date independenceDay("1945/8/15");
-    independenceDay.show();
-    cout << birth.getYear() << ',' << birth.getMonth() << ',' << birth.getDay() << '\n';
-    return 0;
+int main() {
+	Date birth(2014, 3, 20);
+	Date independenceDay("1945/12/15");
+	independenceDay.show();
+	cout << birth.getYear() << "," << birth.getMonth() << "," << birth.getDay() << endl;
 }
