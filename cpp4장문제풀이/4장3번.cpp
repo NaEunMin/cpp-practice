0210bebd7e383a0c6b9fd40aba83ca2b8dbabdeb
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-03-28 13:05:23 +0900

    Update 4장3번.cpp

--- cpp4장문제풀이/4장3번.cpp
+++ cpp4장문제풀이/4장3번.cpp
@@ -1,34 +1,46 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-/*
-
-이번 문제는 at()함수가 문자를 리턴한다는 것에 생각이 고정되어 문제를 바라보는 시각이 좁아졌다..
-
-*/
+// 더 다양한 방법으로 문제를 풀었다.
 
 #include <iostream>
 #include <string>
 using namespace std;
+int main() {
+	string str;
+	int count = 0;
+	int index = 0;
+	cout << "문자열 입력 >>";
+	getline(cin, str);
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == 'a') //문자열 다루는 기본적인 방법
+			count++;
+	}
+	cout << "문자는 " << count << "개 있습니다." << endl;
 
-int main(){
-    string a;
-    int count=0;
-    cout << "문자열 입력>> ";
-    getline(cin,a,'\n');
-    for(int i=0;i<a.length();i++){
-        a[i]=tolower(a[i]);
-        char ch=a.at(i);
-        if(ch=='a'){
-            count++;
-        }
-    }
-    cout << "문자 a는 " << count <<"개 있습니다.";
+	count = 0;
+	for (int i = 0; i < str.length(); i++) {
+		if (str.at(i) == 'a')
+			count++;
+	}
+	cout << "문자는 " << count << "개 있습니다." << endl;
 
-    return 0;
+	count = 0;
+	for (int i = 0; i < str.length(); i++) {
+		if (str.at(i) == 'a') //i인덱스에 있는 문자를 리턴하는 함수 at()
+			count++;
+	}
+	cout << "문자는 " << count << "개 있습니다." << endl;
+
+	count = 0;
+	while (true) {
+		if (str.find('a', index) == -1)
+			break;
+
+		if (str.find('a', index) >= 0) { //a가 있는 인덱스를 반환하는 함수 find()
+			count++;
+			index = str.find('a', index); //이 코드가 없으면 1칸씩 올라가면서 a가 있는 인덱스를
+			//다 반환해서 원하는 값이 안나온다. a를 찾았다면 해당 인덱스를 저장해주자
+		}
+		index++; //a의 다음 인덱스부터 비교하기 위한 코드
+	}
+	cout << "문자는 " << count << "개 있습니다." << endl;
+
 }
