ac56eb680486d0830799ee9ec558273299d499b9
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-04-17 12:02:26 +0900

    Update 6장7번.cpp

--- cpp6장문제풀이/6장7번.cpp
+++ cpp6장문제풀이/6장7번.cpp
@@ -10,7 +10,14 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 저번에도 공부한 내용임-> 알파벳 랜덤 출력 ex) char ch = 'a'+rand()%26;
 공부하면서 안 사실-> 실수 랜덤 출력(0~1) (double)rand()/Rand_MAX
-
+rand()는 정수 0~32767중 랜덤 생성한다는걸 반드시 기억해야한다.
+%는 나누는 수 미만의 나머지만 나오는 구조를 이용하여 nextInt함수의 return 식이 나오는 구조이다
+ex) rand() % 해당 수 전까지 + 해당 수 부터
+실수 랜덤 생성은 rand()와 /의 이해가 필요하다.
+rand()는 위 처럼 0~32767중 생성을 한다.
+그렇다면 rand() / 32767을 하면 몫이 0 아니면 1이 나올 수 밖에 없는 구조이다,
+(double)rand()를 해주면 rand()에서 32767이 나오면 랜덤 생성해서 1이 나오는거고 32767이 나오지 않는다면
+몫이 0이고 double로 형변환을 하였기 때문에 계속 계산을 진행하여 0뒤 소수점이 나오는 구조이다.
 */
 
 #include <iostream>
