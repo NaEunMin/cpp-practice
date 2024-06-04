5e86b0816952f321fdb542a76f0b34e9fd82547b
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-06-04 19:13:16 +0900

    Create 11장11번.cpp

---
+++ cpp11장문제풀이/11장11번.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+istream& pos(istream& ins) {
+	cout << "위치는?";
+	return ins;
+}
+int main() {
+	int x, y;
+	cin >> pos >> x;
+	cin >> pos >> y;
+	cout << x << ',' << y << endl;
+}
