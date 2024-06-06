10ea717e91733ffbc4f8348f877b87bceef79e78
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-06-06 16:38:26 +0900

    Update 7장3번.cpp

--- cpp7장문제풀이/7장3번.cpp
+++ cpp7장문제풀이/7장3번.cpp
@@ -6,34 +6,32 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 
 #include <iostream>
-#include <string>
-
 using namespace std;
-
 class Book {
 	string title;
 	int price, pages;
-public : 
-	Book(string title="", int price=0, int pages=0) {
-		this->title = title; this->price = price; this->pages = pages;
+public:
+	Book(string title = "", int price = 0, int pages = 0) {
+		this->title = title;
+		this->price = price;
+		this->pages = pages;
 	}
 	void show() {
-		cout << title << ' ' << price << "원 " << pages << " 페이지" << endl;
+		cout << title << ' ' << price << "원 " << pages << "페이지 " << endl;
 	}
 	string getTitle() { return title; }
-	
-	bool operator ! (){
-	    if(price==0)
-	    return true;
-	    else
-	    return false;
+	/*
+	bool operator !() {
+		if (price == 0) return true;
+		else return false;
+	}*/
+	friend bool operator !(Book a) {
+		if (a.price == 0) return true;
+		else return false;
 	}
 };
-
-
 int main() {
 	Book book("벼룩시장", 0, 50);
-	if (!book) cout << "공짜다" << endl;
-	
+	if (!book)cout << "공짜다" << endl;
 	return 0;
 }
