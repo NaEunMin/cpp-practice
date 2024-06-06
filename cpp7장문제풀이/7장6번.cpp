986fcbcc7a4e521c5dead2045b1b96bf36139a17
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-06-06 17:28:47 +0900

    Update 7장6번.cpp

--- cpp7장문제풀이/7장6번.cpp
+++ cpp7장문제풀이/7장6번.cpp
@@ -9,6 +9,7 @@ public:
 	void show() {
 		cout << "Matrix = { " << a << ' ' << ' ' << b << ' ' << c << ' ' << d << " }" << endl;
 	}
+	/*
 	Matrix operator + (Matrix b) {
 		Matrix tmp;
 		tmp.a = this->a + b.a;
@@ -27,8 +28,29 @@ public:
 	bool operator == (Matrix b) {
 		if (a == b.a && this->b == b.b && c == b.c && d == b.d) return true;
 		else return false;
-	}
+	}*/
+	friend Matrix operator + (Matrix a, Matrix b);
+	friend void operator += (Matrix& a, Matrix b);
+	friend bool operator == (Matrix a, Matrix b);
 };
+Matrix operator + (Matrix a, Matrix b) {
+	Matrix tmp;
+	tmp.a = a.a + b.a;
+	tmp.b = a.b + b.b;
+	tmp.c = a.c + b.c;
+	tmp.d = a.d + b.d;
+	return tmp;
+}
+void operator += (Matrix& a, Matrix b) {
+	a.a += b.a;
+	a.b += b.b;
+	a.c += b.c;
+	a.d += b.d;
+}
+bool operator == (Matrix a, Matrix b) {
+	if (a.a == b.a && a.b == b.b && a.c == b.c && a.d == b.d)return true;
+	else return false;
+}
 int main() {
 	Matrix a(1, 2, 3, 4), b(2, 3, 4, 5), c;
 	c = a + b;
