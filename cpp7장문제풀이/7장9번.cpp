062926d241afae97b51344bc484cf99c355158c6
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-06-06 18:23:04 +0900

    Update 7장9번.cpp

--- cpp7장문제풀이/7장9번.cpp
+++ cpp7장문제풀이/7장9번.cpp
@@ -42,3 +42,24 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	void show() { cout << "radius = " << radius << " 인 원" << endl; }
+	friend Circle operator + (int a, Circle b);
+};
+Circle operator + (int a, Circle b) {
+	Circle tmp;
+	tmp.radius = a + b.radius;
+	return tmp;
+}
+int main() {
+	Circle a(5), b(4);
+	b = 1 + a;
+	a.show();
+	b.show();
+}
