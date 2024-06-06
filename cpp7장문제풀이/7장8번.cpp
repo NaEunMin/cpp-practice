151a6d40df9d6a19eb2012eb86721527d7239af0
NaEunMin <123241668+NaEunMin@users.noreply.github.com>
2024-06-06 18:20:09 +0900

    Update 7장8번.cpp

--- cpp7장문제풀이/7장8번.cpp
+++ cpp7장문제풀이/7장8번.cpp
@@ -15,51 +15,40 @@ Write your code in this editor and press "Run" button to compile and execute it.
 */
 
 #include <iostream>
-
 using namespace std;
-
-class Circle{
-    int radius;
-    public:
-    Circle(int radius=0){
-        this->radius = radius;
-    }
-    void show() {
-        cout << "radius = " << radius << " 인 원" << endl;
-    }
- /*  
-    Circle& operator ++(){
-        radius++;
-        return *this;
-    }
-    
-    Circle operator ++(int x){
-        Circle tmp = *this;
-        radius++;
-        return tmp;
-    }*/
-    friend Circle& operator ++(Circle &c);
-    friend Circle operator ++(Circle &c, int x);
-
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	void show() { cout << "radius = " << radius << " 인 원" << endl; }
+	Circle& operator ++ ();
+	Circle operator ++ (int x);
+	//friend Circle& operator ++(Circle& op);
+	//friend Circle operator ++ (Circle& op, int x);
 };
-
-Circle& operator ++ (Circle &c){
-    c.radius++;
-    return c;
+Circle& Circle::operator++() {
+	radius++;
+	return *this;
 }
-
-Circle operator ++(Circle &c, int x){
-   Circle tmp = c;
-   c.radius++;
-   return tmp;
+Circle Circle::operator++(int x) {
+	Circle tmp = *this;
+	radius++;
+	return tmp;
 }
-int main()
-{
-    Circle a(5), b(4);
-    ++a;
-    b = a++;
-    a.show();
-    b.show();
-
-    return 0;
+/*
+Circle& operator ++(Circle& op) {
+	op.radius++;
+	return op;
+}
+Circle operator ++(Circle& op, int x) {
+	Circle tmp = op;
+	op.radius++;
+	return tmp;
+}*/
+int main() {
+	Circle a(5), b(4);
+	++a;
+	b = a++;
+	a.show();
+	b.show();
 }
